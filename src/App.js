133fc15073e7d84595c75890c644e58c23c8b558
saint-lag <maiasena.gabriel@gmail.com>
2022-03-09 13:17:00 -0300

    [Requisito 2][Primeiro Commit]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './app.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
@@ -7,20 +8,73 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import Loading from './components/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+    };
+    this.loadingStateHandler = this.loadingStateHandler.bind(this);
+  }
+
+  loadingStateHandler(state) {
+    this.setState({
+      loading: state,
+    });
+  }
+
   render() {
+    const { loading } = this.state;
+    const { loadingStateHandler } = this;
     return (
       <Router>
-        <Switch>
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Login } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Switch>
+            <Route
+              path="/search"
+              render={ () => (
+                <Search loadingStateHandler={ loadingStateHandler } />
+              ) }
+            />
+            <Route
+              path="/album/:id"
+              render={ () => <Album loadingStateHandler={ loadingStateHandler } /> }
+            />
+            <Route
+              path="/favorites"
+              render={ () => (
+                <Favorites loadingStateHandler={ loadingStateHandler } />
+              ) }
+            />
+            <Route
+              path="/profile/edit"
+              render={ () => (
+                <ProfileEdit loadingStateHandler={ loadingStateHandler } />
+              ) }
+            />
+            <Route
+              path="/profile"
+              render={ () => (
+                <Profile loadingStateHandler={ loadingStateHandler } />
+              ) }
+            />
+            <Route
+              exact
+              path="/"
+              render={ () => <Login loadingStateHandler={ loadingStateHandler } /> }
+            />
+            <Route
+              path="*"
+              render={ () => (
+                <NotFound loadingStateHandler={ loadingStateHandler } />
+              ) }
+            />
+          </Switch>
+        )}
       </Router>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './pages-styles/login.css';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">Login</div>
+  constructor() {
+    super();
+    this.state = {
+      btnDisabled: true,
+      userName: '',
+    };
+    this.inputLengthTracker = this.inputLengthTracker.bind(this);
+    this.createUserLocal = this.createUserLocal.bind(this);
+  }
 
+  async createUserLocal() {
+    const { userName } = this.state;
+    const { loadingStateHandler } = this.props;
+    loadingStateHandler(true);
+    await createUser({ name: userName });
+    loadingStateHandler(false);
+  }
+
+  inputLengthTracker(event) {
+    const { value } = event.target;
+    const MINIMUN_INPUT_LENGTH = 3;
+    this.setState({
+      btnDisabled: value.length < MINIMUN_INPUT_LENGTH,
+      userName: value,
+    });
+  }
+
+  render() {
+    const { inputLengthTracker, createUserLocal } = this;
+    const { btnDisabled } = this.state;
+    return (
+      <div data-testid="page-login" id="page-login">
+        <img alt="trybe-tunes-logo" />
+        <form>
+          <input
+            data-testid="login-name-input"
+            id="name-input"
+            placeholder="name"
+            onChange={ inputLengthTracker }
+          />
+          <button
+            id="login-button"
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ btnDisabled }
+            onClick={ createUserLocal }
+          >
+            Button
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  loadingStateHandler: PropTypes.func.isRequired,
+};
 
 export default Login;
